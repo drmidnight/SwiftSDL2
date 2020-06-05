86f7bd31d24651eef7263249ded00ec56c6ec3d5
Corey Morello <drmidnight@gmail.com>
2020-06-05 11:20:21 -0700

    Added SDL_Hint methods. Come up with a better way to handle this.

--- Sources/SwiftSDL2/Classes/SDL2.swift
+++ Sources/SwiftSDL2/Classes/SDL2.swift
@@ -74,11 +74,66 @@ public struct SDL {
         return ramp
     }
 
-    // change this to return a bool instead of the system type. 
-    static func wasInit(system: SDLSystem) -> SDLSystem {
-        return SDLSystem(rawValue: SDL_WasInit(system.rawValue))
+    static func wasInit(system: SDLSystem) -> Bool {
+        let flagValue = SDL_WasInit(0)
+        return (flagValue & system.rawValue) ==  system.rawValue 
+    }
+}
+
+// Potentially unecessary/unsafe functions see SDL docs regarding these
+extension SDL {
+    static func setMainReady() {
+        SDL_SetMainReady()
+    }
+}
+
+
+// Hint functions
+extension SDL {
+    typealias HintCallback = SDL_HintCallback
+    enum HintPriority: UInt32 {
+        case `default` = 0
+        case normal = 1
+        case `override` = 2
     }
 
+    static func setHint(hint: String, value: Int) {
+        var valPtr = Int8(value)
+        SDL_SetHint(hint, &valPtr)
+    }
+
+     static func setHint(hint: String, value: Int, priority: HintPriority) {
+        var valPtr = Int8(value)
+        SDL_SetHintWithPriority(hint, &valPtr, SDL_HintPriority(rawValue:priority.rawValue))
+    }
+
+    static func getHint(hint: String) -> Int8 {
+        var result = SDL_GetHint(hint)
+        defer{ result?.deallocate()}
+        return result?.pointee ?? 0
+    }
+
+    static func getHint(hint: String) -> Bool {
+        let result = SDL_GetHintBoolean(hint, false.SDLBoolValue)
+        return result.toBool
+    }
+
+    static func clearHints() {
+        SDL_ClearHints()
+    }
+
+    // Wrap hint callback more cleanly.
+    static func addHintCallback(name: String, callback: HintCallback?, userData: UnsafeMutableRawPointer) {
+        SDL_AddHintCallback(name, callback, userData)
+    }
+
+    static func deleteHintCallback(name: String, callback: HintCallback?, userData: UnsafeMutableRawPointer){
+        SDL_DelHintCallback(name, callback, userData)
+    }
+    
+}
+
+extension SDL {
     // maybe auto-initialize sdl if it hasnt been already?
     /// Runs the passed in closure until a return and will then proceed to call SDL_Quit()
     static func main(_ closure: ()->()) {
---
+++ Sources/SwiftSDL2/Enums/SDLHint.swift
@@ -0,0 +1,57 @@
+// import CSDL2
+
+// // Check all of these, seems the SDL2 docs are missing some that are in the SDL_hints.h
+// public struct SDLHint {
+//     static let accelerometerAsJoytstick  = SDL_HINT_ACCELEROMETER_AS_JOYSTICK
+//     static let androidApkExpMainFileVer  = SDL_HINT_ANDROID_APK_EXPANSION_MAIN_FILE_VERSION
+//     static let androidApkExpPatchFileVer  = SDL_HINT_ANDROID_APK_EXPANSION_PATCH_FILE_VERSION
+//     static let androidBlockOnPause  = SDL_HINT_ANDROID_BLOCK_ON_PAUSE
+//     static let appleTvControllerUIEvents  = SDL_HINT_APPLE_TV_CONTROLLER_UI_EVENTS
+//     static let appleTvRemoteAllowRotation  = SDL_HINT_APPLE_TV_REMOTE_ALLOW_ROTATION
+//     static let bmpSaveLegacyFormat  = SDL_HINT_BMP_SAVE_LEGACY_FORMAT
+//     static let emscriptenKeyboardElement  = SDL_HINT_EMSCRIPTEN_KEYBOARD_ELEMENT
+//     static let frameBufferAcceleration  = SDL_HINT_FRAMEBUFFER_ACCELERATION
+//     static let gameControllerConfig  = SDL_HINT_GAMECONTROLLERCONFIG
+//     static let grabKeyboard  = SDL_HINT_GRAB_KEYBOARD
+//     static let idleTimerDisabled  = SDL_HINT_IDLE_TIMER_DISABLED
+//     static let imeInternalEditing  = SDL_HINT_IME_INTERNAL_EDITING
+//     static let joystickAllowBgEvents  = SDL_HINT_JOYSTICK_ALLOW_BACKGROUND_EVENTS
+//     static let macBgApp  = SDL_HINT_MAC_BACKGROUND_APP
+//     static let macCtrlClickEmulateRightClick  = SDL_HINT_MAC_CTRL_CLICK_EMULATE_RIGHT_CLICK
+//     static let mouseFocusClickThrough  = SDL_HINT_MOUSE_FOCUS_CLICKTHROUGH
+//     static let mouseRelativeModeWarp  = SDL_HINT_MOUSE_RELATIVE_MODE_WARP
+//     static let noSignalHandlers  = SDL_HINT_NO_SIGNAL_HANDLERS
+//     static let orientations  = SDL_HINT_ORIENTATIONS
+//     static let renderDirect3D11Debug  = SDL_HINT_RENDER_DIRECT3D11_DEBUG
+//     static let renderDirect3DThreadSafe  = SDL_HINT_RENDER_DIRECT3D_THREADSAFE
+//     static let renderDriver  = SDL_HINT_RENDER_DRIVER
+//     static let renderOpenGLShaders  = SDL_HINT_RENDER_OPENGL_SHADERS
+//     static let renderScaleQuality  = SDL_HINT_RENDER_SCALE_QUALITY
+//     static let renderVsync  = SDL_HINT_RENDER_VSYNC
+//     static let rpiVideoLayer  = SDL_HINT_RPI_VIDEO_LAYER
+//     static let threadStackSize  = SDL_HINT_THREAD_STACK_SIZE
+//     static let timerResolution  = SDL_HINT_TIMER_RESOLUTION
+//     static let videoAllowScreenSaver  = SDL_HINT_VIDEO_ALLOW_SCREENSAVER
+//     static let videoHidpiDisabled  = SDL_HINT_VIDEO_HIGHDPI_DISABLED
+//     static let videoMacFullscreenSpaces  = SDL_HINT_VIDEO_MAC_FULLSCREEN_SPACES
+//     static let videoMinimizeOnFocusLoss  = SDL_HINT_VIDEO_MINIMIZE_ON_FOCUS_LOSS
+//     static let videoWindowSharePixelFormat  = SDL_HINT_VIDEO_WINDOW_SHARE_PIXEL_FORMAT
+//     static let videoWinD3DCompiler  = SDL_HINT_VIDEO_WIN_D3DCOMPILER
+//     static let videoX11NetWmPing  = SDL_HINT_VIDEO_X11_NET_WM_PING
+//     static let videoX11Xinerama  = SDL_HINT_VIDEO_X11_XINERAMA
+//     static let videoX11Xrandr  = SDL_HINT_VIDEO_X11_XRANDR
+//     static let videoX11Xvidmode  = SDL_HINT_VIDEO_X11_XVIDMODE
+//     static let windowsDisableThreadNaming  = SDL_HINT_WINDOWS_DISABLE_THREAD_NAMING
+//     static let windowsEnableMessageLoop  = SDL_HINT_WINDOWS_ENABLE_MESSAGELOOP
+//     static let windowsNoCloseOnAltF4  = SDL_HINT_WINDOWS_NO_CLOSE_ON_ALT_F4
+//     static let windowFrameUsableWhileCursorHidden  = SDL_HINT_WINDOW_FRAME_USABLE_WHILE_CURSOR_HIDDEN
+//     static let winRTHandleBackButton  = SDL_HINT_WINRT_HANDLE_BACK_BUTTON
+//     static let winRTPrivacyPolicyLabel  = SDL_HINT_WINRT_PRIVACY_POLICY_LABEL
+//     static let winRTPrivacyPolicyURL  = SDL_HINT_WINRT_PRIVACY_POLICY_URL
+//     static let xinputEnabled  = SDL_HINT_XINPUT_ENABLED
+//     static let xinputUseOldJoystickMapping  = SDL_HINT_XINPUT_USE_OLD_JOYSTICK_MAPPING
+// }
+
+// extension SDLHint {
+
+// }

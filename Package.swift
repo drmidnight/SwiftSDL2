0cafce35f4226b5285d999649bbb5a80191244e3
Corey Morello <drmidnight@gmail.com>
2020-06-19 17:40:11 -0700

    Changed naming

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "SDL2Test",
-     products: [
+    name: "SwiftSDL2",
+    products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "SwiftSDL",
+            name: "SwiftSDL2",
             targets: ["SwiftSDL2"]),
     ],
     dependencies: [

f1ca732f7746eff9d36a78289a0089a1d9f1f997
Corey Morello <drmidnight@gmail.com>
2020-06-21 22:12:12 -0700

    updated dependencies

--- Package.swift
+++ Package.swift
@@ -13,14 +13,14 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/drmidnight/CSDL2.git", from: "0.0.5"),
+        .package(url: "https://github.com/drmidnight/CSDL2.git", from: "0.0.6"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftSDL2",
-            dependencies: []),
+            dependencies: ["CSDL2"]),
         .testTarget(
             name: "SwiftSDL2Tests",
             dependencies: ["SwiftSDL2"]),

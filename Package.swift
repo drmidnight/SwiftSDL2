da878a37d26e8f659e6c96567e25862fdac34edb
Corey <drmidnight@gmail.com>
2020-07-14 13:52:08 -0700

    Testing some linker fixes for macOS

--- Package.swift
+++ Package.swift
@@ -13,14 +13,19 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/drmidnight/CSDL2.git", from: "0.0.10"),
+        .package(url: "https://github.com/drmidnight/CSDL2.git", from: "0.0.11"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftSDL2",
-            dependencies: ["CSDL2"]),
+            dependencies: ["CSDL2"],
+                    linkerSettings: [
+                    .unsafeFlags(["-L", "/usr/local/Cellar/sdl2/2.0.12_1/lib/"], .when(platforms: [.macOS])),
+                    .unsafeFlags(["-L", "/usr/local/Cellar/sdl2_image/2.0.5/lib/"], .when(platforms: [.macOS])),
+                    .unsafeFlags(["-L", "/usr/local/Cellar/sdl2_ttf/2.0.15/lib/"], .when(platforms: [.macOS])),
+                    ]),
     ]
   
 )
